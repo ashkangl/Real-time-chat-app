d2daafd585526596b2f992084c09b9d179dd5bf6
Ashkan Golzad <ashkan72golzad@gmail.com>
2024-11-23 21:12:57 +0330

    final

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Server } from 'socket.io'; // Use named import
+import { Server } from 'socket.io';
 import http from 'http';
 import cors from 'cors'
 
@@ -7,13 +7,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-    origin: 'http://localhost:3000', // Allow requests from your frontend
+    origin: 'https://real-time-chat-app-seven-orcin.vercel.app',
     methods: ['GET', 'POST']
     }
     });
     
 app.use(cors({
-    origin: 'http://localhost:3000' // Allow requests from your frontend
+    origin: 'https://real-time-chat-app-seven-orcin.vercel.app'
 }));
 
 const colors = ['#E1BEE7','#EF9A9A','#B3E5FC','#C5E1A5','#FFF176','#FFAB91','#E53935','#1E88E5','#FDD835']
